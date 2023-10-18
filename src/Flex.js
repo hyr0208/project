dfeb4806fcc12c5011936464eef5b96985d1e393
hyr0208 <hyr0208@naver.com>
2023-10-18 13:19:25 +0900

    translate 값 수정

--- src/Flex.js
+++ src/Flex.js
@@ -4,7 +4,7 @@ function Flex() {
   return (
     <div>
       <div className="bg-[#F07260] w-[100px] h-[100px] relative">
-        <div className="w-[20px] h-[20px] bg-white absolute rounded-[100%] top-[20px] animate-custom-bounce  ml-[40px]"></div>
+        <div className="w-[20px] h-[20px] bg-white absolute rounded-[100%] top-[30px] animate-custom-bounce  ml-[40px]"></div>
         <div
           className={`w-[20px] h-[5px] bottom-5 rounded-[100%] bg-[0,0,0,0.2] animate-custom-shadow absolute`}
           style={{
